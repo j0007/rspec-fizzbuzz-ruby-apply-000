1c9392988c66713b3ce5fa917f0704e8ce5d5e33
John Chen <chenj317@gmail.com>
2020-07-17 19:33:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+       if int % 3 == 0
+        puts "Fizz"
+        return "Fizz"
+        
+        elsif int % 5 == 0
+        puts "Buzz"
+        return "Buzz"
+    
+        if (int % 3) && (int % 5)  == 10
+        puts "FizzBuzz"
+        return  "FizzBuzz"
+        
+  end
+end
+end
+fizzbuzz(15)
